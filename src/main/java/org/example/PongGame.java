ffa66a6cc781d862d67c03b01f1c9efa1fc1cce6
Soemuda <madis.kask@omakase.ee>
2024-02-01 13:48:50 +0200

    Pong game 50% there

--- src/main/java/org/example/PongGame.java
+++ src/main/java/org/example/PongGame.java
@@ -1,11 +1,11 @@
 package org.example;
-//import java.awt.*;
-//import java.awt.event.*;
-//import java.util.*;
-//import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
 
 
-/*public class PongGame {
+public class PongGame {
     public static void main(String[] args) {
         GameFrame frame = new GameFrame();
 
