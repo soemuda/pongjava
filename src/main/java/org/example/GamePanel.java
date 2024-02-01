9f7f93e75642d27030ff45fc5ec534b85d5a2015
Soemuda <madis.kask@omakase.ee>
2024-02-01 13:28:43 +0200

    Pong game 50% there

---
+++ src/main/java/org/example/GamePanel.java
@@ -0,0 +1,93 @@
+package org.example;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+public class GamePanel extends JPanel implements  Runnable{
+    //Final if you dont wanna modify the int somewhere else (also makse the progam run a little bit faster
+    static final int GAME_WIDTH = 1000;
+    static final int GAME_HEIGHT = (int)(GAME_WIDTH * (0.5555));
+    static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH,GAME_HEIGHT);
+    static final int BALL_DIAMETER = 20;
+    static final int PADDLE_WIDTH = 25;
+    static final int PADDLE_HEIGHT = 100;
+    Thread gameThread;
+    Image image;
+    Graphics graphics;
+    Random random;
+    Paddle paddle1;
+    Paddle paddle2;
+    Ball ball;
+    Score score;
+
+    GamePanel() {
+        newPaddle();
+        newBall();
+        score = new Score(GAME_WIDTH,GAME_HEIGHT);
+        this.setFocusable(true);
+        this.addKeyListener(new AL());
+        this.setPreferredSize(SCREEN_SIZE);
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    public void newBall() {
+        paddle1 = new Paddle(0,(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH,PADDLE_HEIGHT,1);
+        paddle2 = new Paddle(GAME_WIDTH-PADDLE_WIDTH,(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH,PADDLE_HEIGHT,2);
+
+    }
+    public void newPaddle() {
+
+    }
+
+    public void paint(Graphics g) {
+        image = createImage(getWidth(),getHeight());
+        graphics = image.getGraphics();
+        draw(graphics);
+        g.drawImage(image,0,0,this);
+
+    }
+    public void draw(Graphics g) {
+        paddle1.draw(g);
+        paddle2.draw(g);
+    }
+    public void move() {
+
+    }
+
+    public void checkCollision() {
+
+    }
+
+    public void run() {
+        long lastTime  = System.nanoTime();
+        double amountOfTicks =60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta =0;
+        while(true) {
+            long now = System.nanoTime();
+            delta += (now -lastTime)/ns;
+            lastTime = now;
+            if(delta >=1) {
+                move();
+                checkCollision();
+                repaint();
+                delta--;
+            }
+        }
+
+    }
+    public class AL extends KeyAdapter{
+        public void keyPressed(KeyEvent e) {
+            paddle1.keyPressed(e);
+            paddle2.keyPressed(e);
+
+        }
+        public void keyReleased(KeyEvent e) {
+            paddle1.keyReleased(e);
+            paddle2.keyReleased(e);
+
+        }
+    }
+
+}
